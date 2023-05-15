8745c29b6f3f6d2a80d9a34aa1db43258ecb20a6
Alexander <alexvanheteren@gmail.com>
2023-05-15 11:56:50 +0200

    navbar en footer gemaakt

--- web/front-end/golftrader/app/layout.tsx
+++ web/front-end/golftrader/app/layout.tsx
@@ -1,9 +1,19 @@
-import './globals.css'
+
+import {Footer} from "@/components/organisms/footer.organism";
+import "./globals.css";
+import { NavBar } from "@/components/organisms/navBar.organism";
+import { Montserrat } from 'next/font/google'
+
 
 export const metadata = {
-  title: 'Golftrader',
-  description: 'S3IP A. van Heteren',
-}
+  title: "Golftrader",
+  description: "S3IP A. van Heteren",
+};
+
+const montserrat = Montserrat({
+  subsets: ['latin']
+})
+
 
 export default function RootLayout({
   children,
@@ -11,8 +21,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={montserrat.className}>
+      <body>
+        <NavBar/>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   )
 }
+
